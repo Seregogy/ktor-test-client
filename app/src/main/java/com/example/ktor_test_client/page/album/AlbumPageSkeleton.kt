ca53817120863393286a30ab6757a98713b68336
Seregogy <delhikovsr@gmail.com>
2025-11-19 15:49:05 +0300

    add shimmer loading skeleton on album page

---
+++ app/src/main/java/com/example/ktor_test_client/page/album/AlbumPageSkeleton.kt
@@ -0,0 +1,145 @@
+package com.example.ktor_test_client.page.album
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.valentinilk.shimmer.shimmer
+
+@Preview(showBackground = true)
+@Composable
+fun AlbumPageSkeleton() {
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+
+            Box(
+                modifier = Modifier
+                    .height(screenHeight * .7f)
+                    .fillMaxWidth()
+                    .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
+                    .drawWithContent {
+                        drawContent()
+
+                        drawRect(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.White.copy(.2f),
+                                    Color.Transparent,
+                                )
+                            ),
+                            blendMode = BlendMode.DstIn
+                        )
+                    }
+                    .background(Color.Gray)
+            )
+    Column(
+        modifier = Modifier
+            .shimmer()
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(screenHeight * .7f)
+                .padding(bottom = 20.dp)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(horizontal = 10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(150.dp)
+                        .height(28.dp)
+                        .clip(MaterialTheme.shapes.small)
+                        .background(Color.Gray)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .width(60.dp)
+                        .height(20.dp)
+                        .clip(MaterialTheme.shapes.small)
+                        .background(Color.Gray)
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier
+                        .fillMaxWidth(.85f)
+                ) {
+                    repeat(4) {
+                        Box(
+                            modifier = Modifier
+                                .size(65.dp)
+                                .clip(CircleShape)
+                                .background(Color.Gray)
+                        )
+                    }
+                }
+            }
+        }
+
+        Spacer(Modifier.height(20.dp))
+
+        Column {
+            repeat(5) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 10.dp)
+                        .padding(vertical = 10.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(12.dp)
+                                .clip(MaterialTheme.shapes.small)
+                                .background(Color.Gray)
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .padding(horizontal = 25.dp)
+                                .height(20.dp)
+                                .width(100.dp)
+                                .clip(MaterialTheme.shapes.small)
+                                .background(Color.Gray)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
